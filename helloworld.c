2f48dabf640c6bc194e22e3644c274840f24a352
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-10-16 10:43:44 -0700

    cleanup commented code

--- helloworld.c
+++ helloworld.c
@@ -8,7 +8,6 @@
 //if the serial monitor doesn't show anything, blindly type the first number, press enter, second number and press enter
 int main() {
     stdio_init_all();
-    //char num1,num2,op;
     int num1,num2,op;
     //num1=6;
     //num2=7;
@@ -20,28 +19,21 @@ int main() {
         printf("Second number is : %d\n",num2);
         printf("Enter operation to be performed: Addition, Difference, Multiplication, Division (1,2,3,4, 5 to exit)\n");
         op=getchar();
-        //scanf("%d",&op);
-        //op = getchar();
         if(op=='1'){
-            //printf("The sum of the two numbers is: %d\n",result_num1+result_num2);
             printf("The sum of the two numbers is: %d\n",num1+num2);
         }
         else if(op=='2'){
-            //printf("The difference of the two numbers is: %d\n",result_num1-result_num2);
             printf("The difference of the two numbers is: %d\n",num1-num2);
         }
         else if(op=='3'){
-            //printf("The multiplication of the two numbers is: %d\n",result_num1*result_num2);
             printf("The multiplication of the two numbers is: %d\n",num1*num2);
         }
         else if(op=='4'){
-            //printf("The division of the two numbers is: %d\n",result_num2/result_num2);
             printf("The division of the two numbers is: %d\n",num2/num2);
         }
         else{
             printf("Exited!\n");
         }
-        //sleep_ms(100);
         return 0;   
     }
 }
