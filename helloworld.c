a29d015a84589a72f4e8bca84afd6a86153af34c
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-10-16 02:13:37 -0700

    add addition and difference

--- helloworld.c
+++ helloworld.c
@@ -11,13 +11,27 @@ void main() {
     stdio_init_all();
     while (true) {
         int num1,num2,op;
-        printf("Enter first number: ");
+        printf("Enter first number: \n");
         scanf("%d",&num1);
-        printf("First number is: %d",num1);
+        printf("First number is: %d\n",num1);
         sleep_ms(1000);
-        printf("Enter second number: ");
+        printf("Enter second number: \n");
         scanf("%d",&num2);
-        printf("Second number is : %d",&num2);
+        printf("Second number is : %d\n",&num2);
+        printf("Enter operation to be performed: Addition, Difference, Multiplication, Division (1,2,3,4, 5 to exit)\n");
+        scanf("%d",&op);
+        if(op==1)
+        {
+            printf("The sum of the two numbers is: %d\n",num1+num2);
+        }
+        else if(op==2)
+        {
+            printf("The difference of the two numbers is: %d\n",num1-num2);
+        }
+        else
+        {
+            printf("Exited!");
+        }
         
     }
 }
