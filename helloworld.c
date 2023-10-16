27447ff263e3a7651ce057b6844c62550b0a331b
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-10-16 08:21:52 -0700

    make printf work again

--- helloworld.c
+++ helloworld.c
@@ -14,18 +14,19 @@ void main() {
         printf("Enter first number: \n");
         num1=getchar();
         //scanf("%d",&num1);
+    int num1_int = num1 - '0';
+    int result_num1 = num1_int + 1;
         printf("First number is: %d\n",num1);
         printf("Enter second number: \n");
         num2=getchar();
         //scanf("%d",&num2);
+    int num2_int = num2 - '0';
+    int result_num2 = num2_int + 1;    
         printf("Second number is : %d\n",&num2);
         printf("Enter operation to be performed: Addition, Difference, Multiplication, Division (1,2,3,4, 5 to exit)\n");
         op=getchar();
         //scanf("%d",&op);
-    int num1_int = num1 - '0';
-    int result_num1 = num1_int + 1;
-    int num2_int = num2 - '0';
-    int result_num2 = num2_int + 1;    
+
         if(op=='1'){
             printf("The sum of the two numbers is: %d\n",result_num1+result_num2);
         }
