658b61813adbf79a6964efa440e43720547a6e1f
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-10-16 07:56:09 -0700

    move to just getchar instead of getchar_timeout_us

--- helloworld.c
+++ helloworld.c
@@ -6,22 +6,22 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-int main() {
+void main() {
     stdio_init_all();
  
     while (true) {
-        sleep_ms(100);
+        //sleep_ms(100);
     int num1,num2,op;
         printf("Enter first number: \n");
-        num1=getchar_timeout_us(1000);
+        num1=getchar();
         //scanf("%d",&num1);
         printf("First number is: %d\n",num1);
         printf("Enter second number: \n");
-        num2=getchar_timeout_us(1000);
+        num2=getchar();
         //scanf("%d",&num2);
         printf("Second number is : %d\n",&num2);
         printf("Enter operation to be performed: Addition, Difference, Multiplication, Division (1,2,3,4, 5 to exit)\n");
-        op=getchar_timeout_us(1000);
+        op=getchar();
         //scanf("%d",&op);
         if(op==1)
         {
