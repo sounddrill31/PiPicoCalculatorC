d8cebae018bc8a7c75a03f67a1770de8188b6edb
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-10-16 09:27:59 -0700

    FIx a small mistake and uncomment scanf op line

--- helloworld.c
+++ helloworld.c
@@ -25,10 +25,10 @@ int main() {
     //int num2_int = num2 - '0';
     //int result_num2 = num2_int + 1;    
         //num2 = getchar();
-        printf("Second number is : %d\n",&num2);
+        printf("Second number is : %d\n",num2);
         printf("Enter operation to be performed: Addition, Difference, Multiplication, Division (1,2,3,4, 5 to exit)\n");
         //op=getchar();
-        //scanf("%d",&op);
+        scanf("%d",&op);
         op = getchar();
         if(op=='1'){
             //printf("The sum of the two numbers is: %d\n",result_num1+result_num2);
