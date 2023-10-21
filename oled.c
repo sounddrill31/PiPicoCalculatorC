fdf0db7e5ab80e69f0166751e3fdbe6a321e9d80
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-10-21 12:02:52 +0530

    rollback code to helloworld

--- oled.c
+++ oled.c
@@ -7,22 +7,11 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico-ssd1306/ssd1306.h"
-#include "pico-ssd1306/textRenderer/TextRenderer.h"
+
 int main() {
     stdio_init_all();
-    i2c_init(I2C_PORT, 1000000); //Use i2c port with baud rate of 1Mhz
-//Set pins for I2C operation
-    gpio_set_function(I2C_PIN_SDA, GPIO_FUNC_I2C);
-    gpio_set_function(I2C_PIN_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_PIN_SDA);
-    gpio_pull_up(I2C_PIN_SCL);
-
-//Create a new display object
-    pico_ssd1306::SSD1306 display = pico_ssd1306::SSD1306(I2C_PORT, 0x3D, pico_ssd1306::Size::W128xH64);
-
-//create a vertical line on x: 64 y:0-63
-    for (int y = 0; y < 64; y++){
-        display.setPixel(64, y);
-}
-    display.sendBuffer(); //Send buffer to device and show on screen
+    while (true) {
+        printf("Hello, world!\n");
+        sleep_ms(1000);
+    }
 }
