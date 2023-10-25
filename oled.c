a59e2306608bfeedb4336af542809ef5303115ad
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-10-24 23:12:13 -0700

    Switch libraries... again.. and again... pt 1

--- oled.c
+++ oled.c
@@ -1,29 +1,109 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 #include "pico/stdlib.h"
-#include "pico-ssd1306/ssd1306.h"
-#include "pico-ssd1306/textRenderer/TextRenderer.h"
 #include "hardware/i2c.h"
 
-// Use the namespace for convenience
-//using namespace pico_ssd1306;
+#include "ssd1306.h"
+#include "example/image.h"
+#include "example/acme_5_outlines_font.h"
+#include "example/bubblesstandard_font.h"
+#include "example/crackers_font.h"
+#include "example/BMSPA_font.h"
 
-int main(){i2c_init(I2C_PORT, 1000000); //Use i2c port with baud rate of 1Mhz
-//Set pins for I2C operation
-gpio_set_function(I2C_PIN_SDA, GPIO_FUNC_I2C);
-gpio_set_function(I2C_PIN_SCL, GPIO_FUNC_I2C);
-gpio_pull_up(I2C_PIN_SDA);
-gpio_pull_up(I2C_PIN_SCL);
+const uint8_t num_chars_per_disp[]={7,7,7,5};
+const uint8_t *fonts[4]= {acme_font, bubblesstandard_font, crackers_font, BMSPA_font};
 
-//Create a new display object
-pico_ssd1306::SSD1306 display = pico_ssd1306::SSD1306(I2C_PORT, 0x3D, pico_ssd1306::Size::W128xH64);
+#define SLEEPTIME 25
 
-    // Here we rotate the display by 180 degrees, so that it's not upside down from my perspective
-    // If your screen is upside down try setting it to 1 or 0
-    display.setOrientation(0);
+void setup_gpios(void);
+void animation(void);
 
-    for (int y = 0; y < 64; y++){
-        drawText(&display, font_12x16, "TEST text", 0 ,0);
+int main() {
+    stdio_init_all();
+
+    printf("configuring pins...\n");
+    setup_gpios();
+
+    printf("jumping to animation...\n");
+    animation();
+
+    return 0;
+}
+
+
+void setup_gpios(void) {
+    i2c_init(i2c1, 400000);
+    gpio_set_function(2, GPIO_FUNC_I2C);
+    gpio_set_function(3, GPIO_FUNC_I2C);
+    gpio_pull_up(2);
+    gpio_pull_up(3);
+}
+
+
+void animation(void) {
+    const char *words[]= {"SSD1306", "DISPLAY", "DRIVER"};
+
+    ssd1306_t disp;
+    disp.external_vcc=false;
+    ssd1306_init(&disp, 128, 64, 0x3C, i2c1);
+    ssd1306_clear(&disp);
+
+    printf("ANIMATION!\n");
+
+    char buf[8];
+
+    for(;;) {
+        for(int y=0; y<31; ++y) {
+            ssd1306_draw_line(&disp, 0, y, 127, y);
+            ssd1306_show(&disp);
+            sleep_ms(SLEEPTIME);
+            ssd1306_clear(&disp);
         }
-    //Send buffer to device and show on screen
-    // Send buffer to the display
-    display.sendBuffer();
+
+        for(int y=0, i=1; y>=0; y+=i) {
+            ssd1306_draw_line(&disp, 0, 31-y, 127, 31+y);
+            ssd1306_draw_line(&disp, 0, 31+y, 127, 31-y);
+            ssd1306_show(&disp);
+            sleep_ms(SLEEPTIME);
+            ssd1306_clear(&disp);
+            if(y==32) i=-1;
+        }
+
+        for(int i=0; i<sizeof(words)/sizeof(char *); ++i) {
+            ssd1306_draw_string(&disp, 8, 24, 2, words[i]);
+            ssd1306_show(&disp);
+            sleep_ms(800);
+            ssd1306_clear(&disp);
+        }
+
+        for(int y=31; y<63; ++y) {
+            ssd1306_draw_line(&disp, 0, y, 127, y);
+            ssd1306_show(&disp);
+            sleep_ms(SLEEPTIME);
+            ssd1306_clear(&disp);
+        }
+
+        for(size_t font_i=0; font_i<sizeof(fonts)/sizeof(fonts[0]); ++font_i) {
+            uint8_t c=32;
+            while(c<=126) {
+                uint8_t i=0;
+                for(; i<num_chars_per_disp[font_i]; ++i) {
+                    if(c>126)
+                        break;
+                    buf[i]=c++;
+                }
+                buf[i]=0;
+
+                ssd1306_draw_string_with_font(&disp, 8, 24, 2, fonts[font_i], buf);
+                ssd1306_show(&disp);
+                sleep_ms(800);
+                ssd1306_clear(&disp);
+            }
+        }
+
+        ssd1306_bmp_show_image(&disp, image_data, image_size);
+        ssd1306_show(&disp);
+        sleep_ms(2000);
+    }
 }
