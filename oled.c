794d16547c70b79198643c3265b8b9d078b1cca2
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-10-24 22:55:23 -0700

    somehow cram C++ code into C LOL

--- oled.c
+++ oled.c
@@ -6,34 +6,24 @@
 // Use the namespace for convenience
 //using namespace pico_ssd1306;
 
-int main(){
-    // Init i2c0 controller
-    i2c_init(i2c0, 1000000);
-    // Set up pins 12 and 13
-    gpio_set_function(12, GPIO_FUNC_I2C);
-    gpio_set_function(13, GPIO_FUNC_I2C);
-    gpio_pull_up(12);
-    gpio_pull_up(13);
+int main(){i2c_init(I2C_PORT, 1000000); //Use i2c port with baud rate of 1Mhz
+//Set pins for I2C operation
+gpio_set_function(I2C_PIN_SDA, GPIO_FUNC_I2C);
+gpio_set_function(I2C_PIN_SCL, GPIO_FUNC_I2C);
+gpio_pull_up(I2C_PIN_SDA);
+gpio_pull_up(I2C_PIN_SCL);
 
-    // If you don't do anything before initializing a display pi pico is too fast and starts sending
-    // commands before the screen controller had time to set itself up, so we add an artificial delay for
-    // ssd1306 to set itself up
-    sleep_ms(250);
-
-    // Create a new display object at address 0x3D and size of 128x64
-    SSD1306 display = SSD1306(i2c0, 0x3D, Size::W128xH64);
+//Create a new display object
+pico_ssd1306::SSD1306 display = pico_ssd1306::SSD1306(I2C_PORT, 0x3D, pico_ssd1306::Size::W128xH64);
 
     // Here we rotate the display by 180 degrees, so that it's not upside down from my perspective
     // If your screen is upside down try setting it to 1 or 0
     display.setOrientation(0);
 
-    // Draw text on display
-    // After passing a pointer to display, we need to tell the function what font and text to use
-    // Available fonts are listed in textRenderer's readme
-    // Last we tell this function where to anchor the text
-    // Anchor means top left of what we draw
-    drawText(&display, font_12x16, "TEST text", 0 ,0);
-
+    for (int y = 0; y < 64; y++){
+        drawText(&display, font_12x16, "TEST text", 0 ,0);
+        }
+    //Send buffer to device and show on screen
     // Send buffer to the display
     display.sendBuffer();
 }
