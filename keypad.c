43169eeae09cea46ab41fab6f349bbb52eced5c0
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-11-09 11:38:08 +0530

    keypad: add some code referencing hhoswaldo's embeddedATM

--- keypad.c
+++ keypad.c
@@ -2,6 +2,7 @@
 #include "hardware/timer.h"
 #include "keypad4x4/pico_keypad4x4.h"
 #include "pico/stdlib.h"
+void gpio_callback(uint gpio, uint32_t event);
 
 uint columns[4] = { 18, 19, 20, 21 };
 uint rows[4] = { 10, 11, 12, 15 };
