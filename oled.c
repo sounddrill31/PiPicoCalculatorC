14dd67a9b05de7660a6c52bc4458cd5fef1f8fcd
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-11-20 22:32:37 +0530

    oled: cleanup unused code

--- oled.c
+++ oled.c
@@ -53,55 +53,13 @@ void animation(void) {
     printf("ANIMATION!\n");
 
     char buf[8];
-
-    for(;;) {
-        for(int y=0; y<31; ++y) {
-            ssd1306_draw_line(&disp, 0, y, 127, y);
-            ssd1306_show(&disp);
-            sleep_ms(SLEEPTIME);
-            ssd1306_clear(&disp);
-        }
-
-        for(int y=0, i=1; y>=0; y+=i) {
-            ssd1306_draw_line(&disp, 0, 31-y, 127, 31+y);
-            ssd1306_draw_line(&disp, 0, 31+y, 127, 31-y);
-            ssd1306_show(&disp);
-            sleep_ms(SLEEPTIME);
-            ssd1306_clear(&disp);
-            if(y==32) i=-1;
-        }
-
         for(int i=0; i<sizeof(words)/sizeof(char *); ++i) {
             ssd1306_draw_string(&disp, 8, 24, 2, words[i]);
             ssd1306_show(&disp);
             sleep_ms(800);
             ssd1306_clear(&disp);
-        }
-
-        for(int y=31; y<63; ++y) {
-            ssd1306_draw_line(&disp, 0, y, 127, y);
-            ssd1306_show(&disp);
-            sleep_ms(SLEEPTIME);
-            ssd1306_clear(&disp);
-        }
-
-        for(size_t font_i=0; font_i<sizeof(fonts)/sizeof(fonts[0]); ++font_i) {
-            uint8_t c=32;
-            while(c<=126) {
-                uint8_t i=0;
-                for(; i<num_chars_per_disp[font_i]; ++i) {
-                    if(c>126)
-                        break;
-                    buf[i]=c++;
-                }
-                buf[i]=0;
-
-                ssd1306_draw_string_with_font(&disp, 8, 24, 2, fonts[font_i], buf);
-                ssd1306_show(&disp);
-                sleep_ms(800);
-                ssd1306_clear(&disp);
-            }
-        }
+        
+        
 
         ssd1306_bmp_show_image(&disp, image_data, image_size);
         ssd1306_show(&disp);
