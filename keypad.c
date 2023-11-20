5a8f318769b642d01a774f59d7ea7dd7e658a529
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-11-20 21:55:53 +0530

    keypad: undo last commit

--- keypad.c
+++ keypad.c
@@ -2,12 +2,9 @@
 #include "hardware/timer.h"
 #include "keypad4x4/pico_keypad4x4.h"
 #include "pico/stdlib.h"
-#include "pico/time.h"
-//void gpio_callback(uint gpio, uint32_t event);
 
 uint columns[4] = { 4, 5, 6, 7 };
 uint rows[4] = { 0, 1, 2, 3 };
-
 char matrix[16] = {
     '1', '2' , '3', 'A',
     '4', '5' , '6', 'B',
@@ -16,28 +13,15 @@ char matrix[16] = {
 };
 
 int main() {
-    int time=1;
-    int endCheckingTime=100000000;
     stdio_init_all();
     pico_keypad_init(columns, rows, matrix);
-    //pico_keypad_irq_enable(true, gpio_callback);
-
     char key;
     while (true) {
         printf("Enter key\n");
-        do { 
-            key = pico_keypad_get_key(); time++; 
-        }while(time < endCheckingTime && key == NULL);
-
-/*      key = pico_keypad_get_key();
-        change_state(key);
-        keypadWait = add_alarm_in_ms(1000, alarm_callback, NULL, true);
-        keypad_irq_enable(false, gpio_callback);  */
+        key = pico_keypad_get_key();
         printf("Key pressed: %c\n", key);
-        busy_wait_us(500000);
-        //break;
+        sleep_us(5000000);
     }
-    busy_wait_us(500000);
 }
 
 //https://github.com/hhoswaldo/pico-keypad4x4
