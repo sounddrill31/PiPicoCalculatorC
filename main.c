248aeb7fa2b67169c5729c9409987abeb36dd9ac
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-11-27 22:01:15 +0530

    final: add tostring function

--- main.c
+++ main.c
@@ -137,7 +137,38 @@ void lcd_init() {
     lcd_send_byte(LCD_DISPLAYCONTROL | LCD_DISPLAYON, LCD_COMMAND);
     lcd_clear();
 }
-
+void tostring(char str[], int num)
+{
+    int i, rem, len = 0, n;
+ 
+    n = num;
+    while (n != 0)
+    {
+        len++;
+        n /= 10;
+    }
+    for (i = 0; i < len; i++)
+    {
+        rem = num % 10;
+        num = num / 10;
+        str[len - (i + 1)] = rem + '0';
+    }
+    str[len] = '\0';
+}
+ 
+int toint(char str[])
+{
+    int len = strlen(str);
+    int i, num = 0;
+ 
+    for (i = 0; i < len; i++)
+    {
+        num = num + ((str[len - (i + 1)] - '0') * pow(10, i));
+    }
+ 
+   return num;
+}
+//credit to https://www.sanfoundry.com/c-program-integer-to-string-vice-versa/
 int main() {
     stdio_init_all();
     pico_keypad_init(columns, rows, matrix);
