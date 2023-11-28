68a223e24cbaf89a58591a77f9232aafb10868c5
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-11-28 10:40:54 +0530

    final: disable most logic to test things out

--- main.c
+++ main.c
@@ -206,7 +206,7 @@ int main() {
                     "Operation"
             };
 
-    while (1) {
+    /*while (1) {
         for (int m = 0; m < sizeof(message) / sizeof(message[0]); m += MAX_LINES) {
             for (int line = 0; line < MAX_LINES; line++) {
                 lcd_set_cursor(line, (MAX_CHARS / 2) - strlen(message[m + line]) / 2);
@@ -263,8 +263,12 @@ int main() {
             sleep_ms(2000);
             lcd_clear();
         }
+    }*/
+    while(1){
+                lcd_string(pico_keypad_get_key_scanner());
+
+
     }
-    
 
     #endif
 }
